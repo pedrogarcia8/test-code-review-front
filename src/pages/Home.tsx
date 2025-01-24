31708140323d230db83e85f3077ad55944d25237
pedrogarcia8 <pedro.jose.garcia8@gmail.com>
2025-01-24 15:52:08 -0300

    Added changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
-const Home: React.FC = () => {
+const Home: FC = () => {
   const [count, setCount] = useState<number>(0);
   const [evenNumbers, setEvenNumbers] = useState<number>(0);
+  const [loopNumber, setLoopNumber] = useState<number>(0);
 
   const handleIncrement = (value: number): void => {
     setCount(value + 1);
   };
 
-  const handleDecrement = (value: number): void => {
-    setCount(value - 1);
+  const handleEvenNumbers = (value: number): void => {
+    setEvenNumbers(evenNumbers + value);
   };
 
-  const handleEvenNumbers = (value: number): void => {
-    setEvenNumbers(value);
+  const handleLoopNumber = (value: number): void => {
+    for (let i = 0; i < 100; i++) {
+      for (let j = 0; j < 100; j++) {
+        setLoopNumber(value * i * j);
+      }
+    }
   };
+
+  useEffect(() => {
+    handleLoopNumber(2);
+  }, [loopNumber]);
 
   return (
     <div style={{ padding: '20px', color: '#000' }}>
@@ -24,7 +33,7 @@ const Home: React.FC = () => {
         <p>Contagem: {count}</p>
         <div style={{ display: 'flex', gap: '10px' }}>
           <button onClick={() => { handleIncrement(count); }}>Aumentar</button>
-          <button onClick={() => { handleDecrement(count); }}>Diminuir</button>
+          <button onClick={() => { setCount(count - 1); }}>Diminuir</button>
         </div>
       </div>
       <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: '#f0f0f0', padding: '20px', borderRadius: '5px', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
@@ -33,7 +42,10 @@ const Home: React.FC = () => {
           <button onClick={() => {handleEvenNumbers(2); }}>Aumentar</button>
           <button onClick={() => {handleEvenNumbers(-2); }}>Diminuir</button>
         </div>
-      </div>  
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: '#f0f0f0', padding: '20px', borderRadius: '5px', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
+        <p>Loop: {loopNumber}</p>
+      </div> 
     </div>
   );
 };
